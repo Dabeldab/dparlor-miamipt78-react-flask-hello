8ba66642a73f67a2f7a02e6c6ca35901454b75d5
Dabeldab <1dareios@gmail.com>
2025-05-28 23:28:49 +0000

    Hello

--- src/api/models.py
+++ src/api/models.py
@@ -9,11 +9,23 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
+    
+    
+class Person(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+
+def serialize(self):
+    return {
+        "id": self.id,
+        "name": self.name,
+    }
--- src/api/routes.py
+++ src/api/routes.py
@@ -14,9 +14,24 @@ CORS(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
+    #We're on the receiving side so we have to make a request to receive from the user.
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
+
+    person = Person()
+    #user = User()
+    #user.email='darius@email.com'
+    #user.password='newpass'
+    #user.is_active=True
+    print('Hello from /hello. Here is your user:')
+    #print(user.serialize)
+    all_people = Person.query.all()
+    all_people = list(map(lambda x: x.serialize(), all_people))
+    #db.session.add(user)
+
+    #translate code to sql and save
+    #db.session.commit()
 
     return jsonify(response_body), 200
